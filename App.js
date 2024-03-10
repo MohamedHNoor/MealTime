14631c0bd4d4e8c7ef2eae7a08d5290ab951f1fa
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-03-10 07:31:05 +0200

    import themes and fonts

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
-import { Searchbar } from 'react-native-paper';
+import { StyleSheet } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import { theme } from './src/infrastructure/theme';
+import { RestaurantScreen } from './src/features/restaurants';
+
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from '@expo-google-fonts/oswald';
+import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-        <View style={{ padding: 16, backgroundColor: 'green' }}>
-          <Searchbar placeholder='Search' />
-        </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: 'blue' }}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <ThemeProvider theme={theme}>
+        <RestaurantScreen />
+      </ThemeProvider>
       <ExpoStatusBar style='auto' />
     </>
   );
