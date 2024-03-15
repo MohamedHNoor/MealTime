7128ecc5d6c0ec6827b2321b00ac3c8a6b33e995
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-03-15 07:55:24 +0200

    Create context state

---
+++ src/services/restaurant/restaurant-context.js
@@ -0,0 +1,12 @@
+import React, { useState, createContext, useEffect, useMemo } from 'react';
+import { restaurantsRequest, restaurantTransform } from './restaurant-service';
+
+export const RestaurantsContext = createContext();
+
+export const RestaurantsContextProvider = ({ children }) => {
+  return (
+    <RestaurantsContext.Provider value={{ restaurants: [1, 2, 3] }}>
+      {children}
+    </RestaurantsContext.Provider>
+  );
+};
