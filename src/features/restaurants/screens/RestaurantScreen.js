179520c61321b16881010c8589643d407b8ef5bf
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-03-15 07:56:09 +0200

    Used context state in restaurant screen component

--- src/features/restaurants/screens/RestaurantScreen.js
+++ src/features/restaurants/screens/RestaurantScreen.js
@@ -1,9 +1,11 @@
+import React, { useContext } from 'react';
 import { FlatList, StatusBar } from 'react-native';
 import { Searchbar } from 'react-native-paper';
 import styled from 'styled-components/native';
 import RestaurantInfoComponent from '../components/RestaurantInfoComponent';
 import { Spacer } from '../../../components/spacer/SpacerComponent';
 import { SafeArea } from '../../../components/utility/SafeAreaComponent';
+import { RestaurantsContext } from '../../../services/restaurant/restaurant-context';
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
@@ -16,29 +18,15 @@ const RestaurantList = styled(FlatList).attrs({
 })``;
 
 const RestaurantScreen = () => {
-  console.log(StatusBar.currentHeight);
+  const restaurantContext = useContext(RestaurantsContext);
+  console.log(restaurantContext);
   return (
     <SafeArea>
       <SearchContainer>
         <Searchbar placeholder='Search' />
       </SearchContainer>
       <RestaurantList
-        data={[
-          { name: 1 },
-          { name: 2 },
-          { name: 3 },
-          { name: 4 },
-          { name: 5 },
-          { name: 6 },
-          { name: 7 },
-          { name: 8 },
-          { name: 9 },
-          { name: 10 },
-          { name: 11 },
-          { name: 12 },
-          { name: 13 },
-          { name: 14 },
-        ]}
+        data={restaurantContext.restaurants}
         renderItem={() => (
           <Spacer position='bottom' size='large'>
             <RestaurantInfoComponent />
