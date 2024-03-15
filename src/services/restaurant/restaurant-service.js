5c8eb62df103071cb4d4487fe434b35794aa9431
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-03-15 07:55:05 +0200

    Fetch data and transform it

---
+++ src/services/restaurant/restaurant-service.js
@@ -0,0 +1,24 @@
+import { mocks } from './mock';
+import camelize from 'camelize';
+
+export const restaurantsRequest = (location = '37.7749295,-122.4194155') => {
+  return new Promise((resolve, reject) => {
+    const mock = mocks[location];
+    if (!mock) {
+      reject('Not Found!');
+    }
+    resolve(mock);
+  });
+};
+
+export const restaurantTransform = ({ results = [] }) => {
+  const mappedResults = results.map((restaurant) => {
+    return {
+      ...restaurant,
+      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+      isClosedTemporarily: restaurant.business_status === 'CLOSED_TEMPORARILY',
+    };
+  });
+  console.log(mappedResults);
+  return camelize(mappedResults);
+};
