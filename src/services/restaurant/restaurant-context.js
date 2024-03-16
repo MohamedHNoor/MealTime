88cf65141706aa2e2251b9fe3675208a176ca25a
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-03-16 13:09:10 +0200

    Fetch data from API

--- src/services/restaurant/restaurant-context.js
+++ src/services/restaurant/restaurant-context.js
@@ -4,8 +4,32 @@ import { restaurantsRequest, restaurantTransform } from './restaurant-service';
 export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = ({ children }) => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const retrieveRestaurants = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      restaurantsRequest()
+        .then(restaurantTransform)
+        .then((results) => {
+          setIsLoading(false);
+          setRestaurants(results);
+        })
+        .catch((error) => {
+          setIsLoading(false);
+          setError(error);
+        });
+    }, 200);
+  };
+
+  useEffect(() => {
+    retrieveRestaurants();
+  }, []);
+
   return (
-    <RestaurantsContext.Provider value={{ restaurants: [1, 2, 3] }}>
+    <RestaurantsContext.Provider value={{ restaurants, isLoading, error }}>
       {children}
     </RestaurantsContext.Provider>
   );
