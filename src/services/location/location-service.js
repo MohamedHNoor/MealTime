bbd29ed1700a298c233934d5bf35aea2fe6a86d7
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-03-18 17:31:41 +0200

    Add functions to request and format locations data

---
+++ src/services/location/location-service.js
@@ -0,0 +1,19 @@
+import camelize from 'camelize';
+import { locations } from './location-mock';
+
+export const locationRequest = (searchTerm) => {
+  return new Promise((resolve, reject) => {
+    const locationMock = locations[searchTerm];
+    if (!locationMock) {
+      reject('Not Found!');
+    }
+    resolve(locationMock);
+  });
+};
+
+export const locationTransform = (result) => {
+  const formatResponse = camelize(result);
+  const { geometry = {} } = formatResponse.results[0];
+  const { lat, lng } = geometry.locations;
+  return { lat, lng };
+};
