0541a52d20529886cc4780696957c54655fe8cc4
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-03-19 11:42:24 +0200

    create location context

---
+++ src/services/location/location-context.js
@@ -0,0 +1,46 @@
+import React, { useState, createContext, useEffect } from 'react';
+import { locationRequest, locationTransform } from './location-service';
+
+export const LocationContext = createContext();
+
+export const LocationContextProvider = ({ children }) => {
+  const [keyword, setKeyword] = useState('San Francisco');
+  const [location, setLocation] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const onSearch = (searchKeyword) => {
+    console.log(searchKeyword);
+    setIsLoading(true);
+    setKeyword(searchKeyword);
+    locationRequest(searchKeyword.toLowerCase())
+      .then(locationTransform)
+      .then((result) => {
+        setIsLoading(false);
+        setLocation(result);
+        console.log(result);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err);
+      });
+  };
+
+  useEffect(() => {
+    onSearch(keyword);
+  }, []);
+
+  return (
+    <LocationContext.Provider
+      value={{
+        keyword,
+        location,
+        isLoading,
+        error,
+        search: onSearch,
+      }}
+    >
+      {children}
+    </LocationContext.Provider>
+  );
+};
