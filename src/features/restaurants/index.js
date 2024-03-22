ae15b4e41bc9b713b64f4f946f6fad4979735b67
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-03-23 00:45:26 +0200

    Add detail feature

--- src/features/restaurants/index.js
+++ src/features/restaurants/index.js
@@ -1,2 +1,3 @@
 export { default as RestaurantInfoComponent } from './components/RestaurantInfoComponent';
 export { default as RestaurantScreen } from './screens/RestaurantScreen';
+export { default as RestaurantDetailScreen } from './screens/RestaurantDetailScreen';
---
+++ src/features/restaurants/screens/RestaurantDetailScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+const RestaurantDetailScreen = () => {
+  return (
+    <View>
+      <Text>RestaurantDetailScreen</Text>
+    </View>
+  );
+};
+export default RestaurantDetailScreen;
