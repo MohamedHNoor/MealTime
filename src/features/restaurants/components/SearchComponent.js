4cf9003c3a8264e8c0112926658bdb85ba470a06
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-03-22 06:01:00 +0200

    Create Search component

---
+++ src/features/restaurants/components/SearchComponent.js
@@ -0,0 +1,27 @@
+import React, { useContext, useState } from 'react';
+import { View, Text } from 'react-native';
+import styled from 'styled-components/native';
+import { Searchbar } from 'react-native-paper';
+import { LocationContext } from '../../../services/location/location-context';
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const SearchComponent = () => {
+  const { keyword, search } = useContext(LocationContext);
+  const [searchKeyword, setSearchKeyword] = useState(keyword);
+  return (
+    <SearchContainer>
+      <Searchbar
+        placeholder='Search'
+        value={searchKeyword}
+        onSubmitEditing={() => search(searchKeyword)}
+        onChangeText={(text) => {
+          setSearchKeyword(text);
+        }}
+      />
+    </SearchContainer>
+  );
+};
+export default SearchComponent;
