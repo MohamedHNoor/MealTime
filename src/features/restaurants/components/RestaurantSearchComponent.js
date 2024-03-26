6556405e153eb850a940b33dda58605c9c26b71c
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-03-26 18:37:50 +0200

    Rename Restaurant search component

---
+++ src/features/restaurants/components/RestaurantSearchComponent.js
@@ -0,0 +1,32 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import styled from 'styled-components/native';
+import { Searchbar } from 'react-native-paper';
+import { LocationContext } from '../../../services/location/location-context';
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantSearchComponent = () => {
+  const { keyword, search } = useContext(LocationContext);
+  const [searchKeyword, setSearchKeyword] = useState(keyword);
+
+  useEffect(() => {
+    setSearchKeyword(keyword);
+  }, [keyword]);
+
+  return (
+    <SearchContainer>
+      <Searchbar
+        placeholder='Search'
+        value={searchKeyword}
+        onSubmitEditing={() => search(searchKeyword)}
+        onChangeText={(text) => {
+          setSearchKeyword(text);
+        }}
+      />
+    </SearchContainer>
+  );
+};
+export default RestaurantSearchComponent;
