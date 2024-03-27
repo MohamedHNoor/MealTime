a7ca1c5c2259cc23297f3c6c6ba987ff6141708a
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-03-27 15:28:01 +0200

    Add the map region

--- src/features/map/screens/MapScreen.js
+++ src/features/map/screens/MapScreen.js
@@ -2,17 +2,43 @@ import React, { useContext, useState, useEffect } from 'react';
 import MapView from 'react-native-maps';
 import styled from 'styled-components/native';
 import MapSearchComponent from '../components/MapSeachComponent';
+import { LocationContext } from '../../../services/location/location-context';
+import { RestaurantsContext } from '../../../services/restaurant/restaurant-context';
 
-const Map = styled(MapView)`
+const StyledMap = styled(MapView)`
   height: 100%;
   width: 100%;
 `;
 
 const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+
+  const [latDelta, setLatDelta] = useState(0);
+
+  const { lat, lng, viewport } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
+
   return (
     <>
       <MapSearchComponent />
-      <Map />
+      <StyledMap
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return null;
+        })}
+      </StyledMap>
     </>
   );
 };
