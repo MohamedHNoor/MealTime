8b9e44468bd79eb13e6d84744bb6ef057a1d42b6
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-03-30 04:43:00 +0200

    Add favourites context provider

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { RestaurantsContextProvider } from './src/services/restaurant/restaurant-context';
 import { LocationContextProvider } from './src/services/location/location-context';
+import { FavouritesContextProvider } from './src/services/favourites/favourites.context';
 import { theme } from './src/infrastructure/theme';
 
 import {
@@ -28,11 +29,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <LocationContextProvider>
-          <RestaurantsContextProvider>
-            <Navigation />
-          </RestaurantsContextProvider>
-        </LocationContextProvider>
+        <FavouritesContextProvider>
+          <LocationContextProvider>
+            <RestaurantsContextProvider>
+              <Navigation />
+            </RestaurantsContextProvider>
+          </LocationContextProvider>
+        </FavouritesContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style='auto' />
     </>
