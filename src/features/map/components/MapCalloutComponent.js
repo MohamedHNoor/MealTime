5b28030000dc69c91479831aa462750a5d0e7671
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-04-04 12:18:06 +0200

    Fix android bug

--- src/features/map/components/MapCalloutComponent.js
+++ src/features/map/components/MapCalloutComponent.js
@@ -1,6 +1,6 @@
 import CompactRestaurantInfo from '../../../components/restaurant/CompactRestaurantInfo';
 
 const MapCalloutComponent = ({ restaurant }) => {
-  return <CompactRestaurantInfo restaurant={restaurant} />;
+  return <CompactRestaurantInfo isMap restaurant={restaurant} />;
 };
 export default MapCalloutComponent;
