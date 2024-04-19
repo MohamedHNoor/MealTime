5c703ffdb735c929a14402cace128e504ef6546d
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-04-19 07:27:21 +0200

    Add styles to Account screens

---
+++ src/features/account/components/account.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const AccountBackground = styled.ImageBackground.attrs({
+  source: require('../../../../assets/home_bg.jpg'),
+})`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const AccountCover = styled.View`
+  position: absolute;
+  width: 100%;
+  heigh: 100%;
+  background-color: rgba(255, 255, 255, 0.3);
+`;
