1d68551edb0b39545776ef0290c54b7044f765c5
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-04-19 07:32:49 +0200

    Fix a bug

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -11,6 +11,6 @@ export const AccountBackground = styled.ImageBackground.attrs({
 export const AccountCover = styled.View`
   position: absolute;
   width: 100%;
-  heigh: 100%;
+  height: 100%;
   background-color: rgba(255, 255, 255, 0.3);
 `;
