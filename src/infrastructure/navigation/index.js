9ba09cd4de65f89ed348db355a652090955deaa5
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-04-19 07:28:53 +0200

    Add navigation container

--- src/infrastructure/navigation/index.js
+++ src/infrastructure/navigation/index.js
@@ -1,5 +1,16 @@
+import React, { useContext } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
 import AppNavigator from './app-navigator';
+import AccountNavigator from './account-navigator';
+
+import { AuthenticationContext } from '../../services/athentication/athentication.context';
 
 export const Navigation = () => {
-  return <AppNavigator />;
+  const { isAuthenticated } = useContext(AuthenticationContext);
+  return (
+    <NavigationContainer>
+      {isAuthenticated ? <AppNavigator /> : <AccountNavigator />}
+    </NavigationContainer>
+  );
 };
