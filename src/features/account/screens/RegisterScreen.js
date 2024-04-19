4f8289fe57094981c89f80cf14e65740d24232df
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-04-19 07:27:59 +0200

    Create register screen

---
+++ src/features/account/screens/RegisterScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import { AccountBackground, AccountCover } from '../components/account.styles';
+
+const RegisterScreen = () => {
+  return (
+    <AccountBackground>
+      <AccountCover />
+    </AccountBackground>
+  );
+};
+export default RegisterScreen;
