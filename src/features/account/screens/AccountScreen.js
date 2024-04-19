3f0d91eee2fdb8c8e1d08ab04658d5069c4a5643
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-04-19 08:26:13 +0200

    Add acount container and Buttons

--- src/features/account/screens/AccountScreen.js
+++ src/features/account/screens/AccountScreen.js
@@ -1,10 +1,34 @@
 import { View, Text } from 'react-native';
-import { AccountBackground, AccountCover } from '../components/account.styles';
+import {
+  AccountBackground,
+  AccountContainer,
+  AccountCover,
+  AuthButton,
+} from '../components/account.styles';
+import { Spacer } from '../../../components/spacer/SpacerComponent';
 
-const AccountScreen = () => {
+const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <AccountContainer>
+        <AuthButton
+          icon='lock-open-outline'
+          mode='contained'
+          onPress={() => navigation.navigate('Login')}
+        >
+          Login
+        </AuthButton>
+        <Spacer size='large'>
+          <AuthButton
+            icon='lock-open-outline'
+            mode='contained'
+            onPress={() => navigation.navigate('Register')}
+          >
+            Register
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
     </AccountBackground>
   );
 };
