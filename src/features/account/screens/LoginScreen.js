5f5d6f98851539a30c8031023f5f81e7b3ab87be
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-04-19 07:27:48 +0200

    Create login screen

---
+++ src/features/account/screens/LoginScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import { AccountBackground, AccountCover } from '../components/account.styles';
+const LoginScreen = () => {
+  return (
+    <AccountBackground>
+      <AccountCover />
+    </AccountBackground>
+  );
+};
+export default LoginScreen;
