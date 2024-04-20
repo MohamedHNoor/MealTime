c6a463fc16d74802ef85a878b0206e455533805c
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-04-20 07:09:28 +0200

    Add isAuthenticated

--- src/services/athentication/athentication.context.js
+++ src/services/athentication/athentication.context.js
@@ -20,7 +20,9 @@ export const AuthenticationContextProvider = ({ children }) => {
   };
 
   return (
-    <AuthenticationContext.Provider value={{ user, isLoading, error, onLogin }}>
+    <AuthenticationContext.Provider
+      value={{ isAuthenticated: !!user, user, isLoading, error, onLogin }}
+    >
       {children}
     </AuthenticationContext.Provider>
   );
